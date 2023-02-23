00a3d4919ecc8ffa4036fcfa96c0f4d18ef3d8b1
Marco <Marco@yfdc.net>
2023-02-23 15:17:34 +0800

    'add_captcha'

--- blog/users/urls.py
+++ blog/users/urls.py
@@ -1,8 +1,11 @@
 # 通过 users 子应用的视图路由
 from django.urls import path
-from users.views import RegisterView
+from users.views import RegisterView,ImageCodeView
 urlpatterns = [
     # path的第一个参数，路由
     # path的第二个参数，视图函数名
     path('register/',RegisterView.as_view(),name='register'),
+
+    #图片验证码的路由
+    path('imagecode/',ImageCodeView.as_view(),name='imagecode'),
 ]
